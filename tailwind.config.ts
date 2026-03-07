324bd8b7d0addc5d9c1ce9c2847622d696ebd3fe
dev-hmo <dev.hlaingminoo@gmail.com>
2026-03-07 22:10:56 +0630

    fix: add tailwind safelist for dynamic db-driven grid column classes, enable full-width merged bento card

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,13 @@ const config: Config = {
     "./src/**/*.{ts,tsx}",
   ],
   darkMode: "class",
+  safelist: [
+    // These classes are used dynamically from DB data and must be safelisted
+    "lg:col-span-1", "lg:col-span-2", "lg:col-span-3", "lg:col-span-4", "lg:col-span-5", "lg:col-span-6",
+    "md:col-span-1", "md:col-span-2", "md:col-span-3", "md:col-span-4", "md:col-span-5", "md:col-span-6",
+    "md:row-span-1", "md:row-span-2", "md:row-span-3", "md:row-span-4",
+    "lg:min-h-[60vh]", "lg:min-h-[50vh]",
+  ],
   theme: {
     extend: {
       animation: {
